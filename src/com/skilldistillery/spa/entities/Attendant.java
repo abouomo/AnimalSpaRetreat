21ff48b4b6a4f0d2d0549be9bb8b0171919a8596
Abner Bouomo <abnerbouomo@gmail.com>
2024-09-21 14:57:50 -0400

    AnimalRetreat and Attendant started

---
+++ src/com/skilldistillery/spa/entities/Attendant.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.spa.entities;
+
+public class Attendant {
+	private String name;
+	
+	public Attendant(String name) {
+		this.name = name;
+	}
+}
--- src/com/skilldistillery/spa/entities/Xenomorph.java
+++ src/com/skilldistillery/spa/entities/Xenomorph.java
@@ -1,11 +1,28 @@
 package com.skilldistillery.spa.entities;
 
 public class Xenomorph extends Animal {
+	private String noise = "Sizzle";
+	
 	
 	public Xenomorph() {}
 	
 	public Xenomorph(String name) {
 		super(name);
 	}
+
+	@Override
+	public void makeNoise() {
+		// TODO Auto-generated method stub
+		super.makeNoise();
+		System.out.println(this.noise);
+	}
+
+	@Override
+	public void eat(int quantity) {
+		// TODO Auto-generated method stub
+		super.eat(quantity);
+	}
+	
+	
 	
 }
