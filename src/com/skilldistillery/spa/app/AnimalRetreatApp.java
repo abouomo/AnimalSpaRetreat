46b8ad51ff9ad1c9bd9feac7975157fc365f3d4d
Abner Bouomo <abnerbouomo@gmail.com>
2024-09-28 00:53:37 -0400

    app started: can display menu. option 1 okay

--- src/com/skilldistillery/spa/app/AnimalRetreatApp.java
+++ src/com/skilldistillery/spa/app/AnimalRetreatApp.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.spa.app;
 
+import java.util.Scanner;
+
 import com.skilldistillery.spa.entities.Animal;
 import com.skilldistillery.spa.entities.AnimalRetreat;
 import com.skilldistillery.spa.entities.Dragon;
@@ -7,6 +9,9 @@ import com.skilldistillery.spa.entities.Hippo;
 import com.skilldistillery.spa.entities.Xenomorph;
 
 public class AnimalRetreatApp {
+	
+	private AnimalRetreat retreat;
+	private Scanner scanner;
 
 	public static void main(String[] args) {
 		AnimalRetreatApp app = new AnimalRetreatApp();
@@ -14,38 +19,72 @@ public class AnimalRetreatApp {
 		app.run();
 	}
 	
+	public void printHead() {
+		System.out.println("We're excited to announce the COMPLETION of "
+				+ "building out our NEW Animal Retreat and Spa Resort.\n"
+				+ "We welcome our non-human guests, and invite them all "
+				+ "to luxuriate in delight!\n"
+				+ "We've hired the best attendant on the planet to provide"
+				+ " for all our guests. ");
+		
+		System.out.println(retreat.getAttendant().getName() 
+				+ " , please introduce yourself!\n");
+		
+		System.out.println("Huzzah! I got the job of my dreams as "
+				+ "Attendant JUST BEFORE THE COMPLETION OF THE NEW Animal"
+				+ " Retreat and Spa! I can't wait to get started!\n");
+	}
+	
+	public void animalsPresentation() {
+		Animal ethel = new Xenomorph("Ethel");
+		Animal miranda = new Dragon("Miranda");
+		Animal henri = new Hippo("Henri");
+		
+		retreat.getAnimals()[4] = ethel;
+		retreat.getAnimals()[7] = miranda;
+		retreat.getAnimals()[0] = henri;
+		System.out.println("Make yourself at home, " +  ethel.getName());
+		System.out.println("Make yourself at home, " +  miranda.getName());
+		System.out.println("Make yourself at home, " +  henri.getName());
+	}
+	
+	public void menu() {
+		System.out.println("\nPlease select an option (1-4) from the menu below:");
+		System.out.println("1. List all animals\n"
+				+ "2. Add an animal\n"
+				+ "3. Start attendant rounds\n"
+				+ "4. Close the inn for the night");
+		System.out.println("Selection: ");
+		
+		int option = scanner.nextInt();
+		if (option == 1) {
+			retreat.listAnimals();
+		} else if (option == 2) { 
+			// FIX ME
+			retreat.addAnimal(null);
+		} else if (option == 3) {
+			retreat.startAttendantRounds();
+		} else if (option == 4) {
+			// FIX ME
+		}
+	}
+	
 	public void run() {
-		AnimalRetreat retreat = new AnimalRetreat("Fred the Attendant");
+		retreat = new AnimalRetreat("Fred the Attendant");
+		scanner = new Scanner(System.in);
 		
-		Animal xeno = new Xenomorph("xenooo");
+		printHead();
+		animalsPresentation();
 		
-		Animal dragon = new Dragon("dragooon");
-		
-		for (int i = 0; i < 10; i++) {
-			(retreat.getAnimals())[i] = xeno;
+		boolean menu = true;
+		while(menu) {
+			menu();
+			
+			menu = false;
 		}
 		
-//		(retreat.getAnimals())[9] = dragon;
-		
-		System.out.println("Test makeRounds() here");
-		retreat.startAttendantRounds();
-		System.out.println("=================================================");
-//		for (Animal animal : retreat.getAnimals()) {
-//			System.out.println(animal);
-//		}
-		
-		System.out.println("=================================================");
-		
-		Animal hippo = new Hippo("hippooo");
-		
-		retreat.addAnimal(hippo);
-		
-		System.out.println("=================================================");
-//		for (Animal animal : retreat.getAnimals()) {
-//			System.out.println(animal);
-//		}
-		
-		
+		scanner.close();
+	
 	}
 
 }
